a00d852e3410d3201ba794a056461c251372f548
ArgenisValdez <argenisvalde80@gmail.com>
2024-10-28 15:57:10 -0400

    Primera parte del proyecto

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const taskForm = document.getElementById("task-form");
+
+const taskList = document.getElementById("task-list");
+
+
+taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("task-input");
+
+    const task = taskInput.value;
+
+    if(task){
+        taskList.append(createTaskElement(task));
+        taskInput.value = "";
+    }
+});
+
+function createTaskElement(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    li.append(createButton("❌", "delete-btn"), createButton("📃", "edit-btn"));
+    return li;
+  }
+  
+  function createButton(text, className) {
+    const btn = document.createElement("span");
+    btn.textContent = text;
+    btn.className = className;
+    return btn;
+  }
+
+  function deleteTask(taskItem){
+    if(confirm("Esta suguro de borrar este elemento?")){
+        taskItem.remove();
+    }
+  }
+
+  taskList.addEventListener("click", (event)=>{
+    if(event.target.classList.contains("delete-btn")){
+        deleteTask(event.target.parentElemnt)
+    }
+  });
+
+  
